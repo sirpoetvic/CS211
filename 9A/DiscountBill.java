34ccf105bfd1871a890a8dd84511518859b674fc
sirpoetvic <sirpoetvic@gmail.com>
2024-01-07 11:51:54 -0800

    Added comments to 9A

--- 9A/DiscountBill.java
+++ 9A/DiscountBill.java
@@ -1,12 +1,22 @@
+/*
+ * {Victor}
+ * Winter 2023, C211, William Iverson
+ * 1/7/24
+ * Chapter 9a Assignment
+ * Data structure class for a Discount Bill
+ */
 public class DiscountBill extends GroceryBill {
+    //Privated data fields
     private int discountCount;
     private double discountAmount;
     private boolean isPreferred;
 
+    //Default Constructor
     public DiscountBill() {
         this(new Employee(), false);
     }
 
+    //Main Constructor
     public DiscountBill(Employee clerk, boolean preferred) {
         super(clerk);
         discountCount = 0;
@@ -14,6 +24,8 @@ public class DiscountBill extends GroceryBill {
         isPreferred = preferred;
     }
 
+    //Overrides the add method from GroceryBill, keeps track of number of discounted
+    //items and the discount amount
     @Override
     public void add(Item i) {
         super.add(i);
@@ -25,11 +37,13 @@ public class DiscountBill extends GroceryBill {
     }
 
 
+    //Overrides the total method from GroceryBill to get a discounted total
     @Override
     public double getTotal() {
         return super.getTotal() - discountAmount;
     }
 
+    //Getter methods
     public int getDiscountCount() {
         return discountCount;
     }
